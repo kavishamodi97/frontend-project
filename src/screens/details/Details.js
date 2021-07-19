50fc681d56b56fc900522e1d9769953741d7c1a2
kavishamodi97 <kavishamodi971@gmail.com>
2021-07-19 18:37:13 +0530

    Render Menu Items In Items Row

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -3,6 +3,7 @@ import './Details.css';
 import Header from '../../common/header/Header';
 import Typography from '@material-ui/core/Typography';
 import 'font-awesome/css/font-awesome.min.css';
+import Divider from '@material-ui/core/Divider';
 
 class Details extends Component {
 
@@ -69,27 +70,27 @@ class Details extends Component {
                             {this.state.restaurantDetails.restaurant_name}
                         </Typography>
                         <br />
-                        <Typography variant="h7">
+                        <Typography variant="h6">
                             {this.state.addressDetails.locality}
                         </Typography>
                         <br />
-                        <Typography variant="h7">
+                        <Typography variant="h6">
                             <span>{categoryNames.join(",")}</span>
                         </Typography>
                         <br />
                         <div className="details-container">
                             <div>
-                                <Typography variant="h7" style={{ fontWeight: 'bold' }}>
+                                <Typography variant="h6" style={{ fontWeight: 'bold', paddingLeft: '3' }}>
                                     <i className="fa fa-star" aria-hidden="true"></i>
                                     {" " + this.state.restaurantDetails.customer_rating}
                                 </Typography>
                                 <Typography color="textSecondary">
                                     {"AVERAGE RATING BY"}<br />
-                                    <span style={{ fontWeight: 'bold' }}>{this.state.restaurantDetails.number_customers_rated}</span>
+                                    <span style={{ fontWeight: 'bold', paddingTop: '3' }}>{this.state.restaurantDetails.number_customers_rated}</span>
                                     {" CUSTOMERS"}
                                 </Typography>
                             </div>
-                            <div>
+                            <div className="rating-section">
                                 <i className="fa fa-inr" aria-hidden="true"></i>
                                 {" " + this.state.restaurantDetails.average_price}
                                 <Typography color="textSecondary">
@@ -101,8 +102,33 @@ class Details extends Component {
                         </div>
                     </div>
                 </div>
-            </div>
+                <div className="menu-cart-section">
+                    <div className="menu-items">
+                        {this.state.categories.map((category, index) => (
+                            <span key={"category" + index}>{category.category_name.toUpperCase()}
+                                <Divider />
+                                {category.item_list.map((items, itemIndex) => (
+                                    <div className="menu-item-row" key={"item-row" + items.item_name + "-" + index + "-" + itemIndex}>
+                                        {items.item_type === "VEG" ?
+                                            <i key={"veg-" + items.item_name} className="fa fa-circle" aria-hidden="true" style={{ color: 'green', marginRight: '15px' }}></i>
+                                            : <i key={"nonveg-" + items.item_name} className="fa fa-circle" aria-hidden="true" style={{ color: 'red', marginRight: '15px' }}></i>
+                                        }
+                                        <span>
+                                            {
 
+                                                items.item_name.split(" ").map((i, menuItemIndex) => (
+                                                    <span key={"item-" + items.item_name + "-" + menuItemIndex}>
+                                                        {i.charAt(0).toUpperCase() + i.slice(1) + " "}</span>
+                                                ))
+                                            }
+                                        </span>
+                                    </div>
+                                ))}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+            </div>
         )
     }
 }
