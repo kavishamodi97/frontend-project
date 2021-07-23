382250a11042b00c1dc6df8b36e503cbf169bd17
kavishamodi97 <kavishamodi971@gmail.com>
2021-07-23 12:58:12 +0530

    Created Order Summary Section

---
+++ src/screens/checkout/ordersummary/OrderSummary.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './OrderSummary.css';
+
+class OrderSummary extends Component {
+    render() {
+        return (
+            <div>
+                Order Summary Section
+            </div>
+        )
+    }
+}
+
+export default OrderSummary;
